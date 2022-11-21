2c13f810635fba6e3cdf0e834402c8c9db0d5a26
cr9169 <bar.gabay.home@gmail.com>
2022-11-21 04:34:43 -0800

    docs: TODO comments got removed

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,5 @@ import { Server } from './server';
 // import { config } from './config';
 
 ((): void => {
-  // TODO: Change to Server connection and maybe check if answers
-  // and questions services are up and user logger accordingly!!!
   Server.startServer();
 })();
