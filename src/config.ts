6b5519f6d32e08dbcc1aac4da2430e379b9bb9df
cr9169 <bar.gabay.home@gmail.com>
2022-11-22 00:01:13 -0800

    feat: server listening port changed to 3002

--- src/config.ts
+++ src/config.ts
@@ -6,14 +6,13 @@ dotenv.config();
 export const config = {
   server: {
     name: 'Questions-crud',
-    port: env.get('APPLICATION_PORT').default(3000).asPortNumber(),
+    port: env.get('APPLICATION_PORT').default(3002).asPortNumber(),
   },
   db: {
-    connectionString: env.get('DB_CONNECTION_URL')
+    connectionString: env
+      .get('DB_CONNECTION_URL')
       .default('mongodb://localhost:27017')
       .asString(),
-    dbName: env.get('QUESTION_DB_NAME')
-      .default('questions-crud')
-      .asString(),
+    dbName: env.get('QUESTION_DB_NAME').default('questions-crud').asString(),
   },
 };
