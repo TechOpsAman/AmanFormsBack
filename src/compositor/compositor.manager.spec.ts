6045301ae3a1f0197930c050a626855b9c5a15ef
cr9169 <bar.gabay.home@gmail.com>
2022-11-24 13:48:46 +0200

    feat: finished manager tests

--- src/compositor/compositor.manager.spec.ts
+++ src/compositor/compositor.manager.spec.ts
@@ -7,7 +7,13 @@ import {
   ISurveyAnswers,
   ISurveyQuestions,
   IQuestion,
+  ISection,
 } from './interfaces/compositor.interface';
+import { testsValues } from '../utils/mocks';
+import {
+  SurveyQuestionsNotFoundError,
+  SurveyAnswersNotFoundError,
+} from '../utils/errors/compositor';
 
 const {
   db: { connectionString, dbName },
@@ -22,19 +28,30 @@ const createSurvey = async (
   surveyName: string,
   creatorId: string,
   content: Array<IQuestion>,
-  newSurvey: ISurveyAnswers
-): Promise<(ISurveyQuestions | ISurveyAnswers)[]> => {
-  const surveyQuestions = await axios
+  newSurvey: Omit<ISurveyAnswers, 'surveyId'>
+): Promise<(ISurveyQuestions | ISurveyAnswers)[] | null> => {
+  const surveyQuestions: ISurveyQuestions | null = await axios
     .post(
       `${questionsServiceAPI}`,
       { surveyName: surveyName, creatorId: creatorId, content: content },
       headers
     )
     .then((res) => res.data);
-  const surveyAnswers = await axios
-    .post(`${answersServiceAPI}`, newSurvey, headers)
+
+  if (!surveyQuestions) return null;
+
+  const newSurveyAnswers: ISurveyAnswers = {
+    surveyId: (surveyQuestions as ISurveyQuestions).id!,
+    ...newSurvey,
+  };
+
+  const surveyAnswers: ISurveyAnswers | null = await axios
+    .post(`${answersServiceAPI}`, { newSurveyAnswers }, headers)
     .then((res) => res.data);
-  return [surveyQuestions, surveyAnswers];
+
+  if (!surveyAnswers) return null;
+
+  return [surveyQuestions as ISurveyQuestions, surveyAnswers as ISurveyAnswers];
 };
 
 describe('Compositor Manager Module', () => {
@@ -53,28 +70,79 @@ describe('Compositor Manager Module', () => {
   });
 
   describe('Delete a survey', () => {
-    test('Should create a survey', async () => {
+    test('Should delete survey`s questions and answers', async () => {
       const createdSurvey = await createSurvey(
         {},
-        validSurveyName1,
-        validCreatorId,
-        validContent1 as IQuestion[],
-        validSurvey1
+        testsValues.questionsValues.validSurveyName1,
+        testsValues.questionsValues.validCreatorId,
+        testsValues.questionsValues.validContent1 as IQuestion[],
+        {
+          answersContent: testsValues.answersValues.validSurvey
+            .content as Array<ISection>,
+          userId: testsValues.answersValues.validSurvey.userId as string,
+        } as Omit<ISurveyAnswers, 'surveyId'>
       );
 
       if (!createdSurvey) {
         fail();
       }
 
-      const survey = await QuestionManager.deleteSurveyById(createdSurvey.id);
-      expect(createdSurvey.id).toEqual(survey?.id);
+      const deletedSurvey = await CompositorManager.deleteSurvey(
+        (createdSurvey[0] as ISurveyQuestions).id!
+      );
+      expect((createdSurvey[0] as ISurveyQuestions).id).toEqual(
+        (deletedSurvey[0] as ISurveyQuestions).id
+      );
     });
 
-    test('Should throw survey not found error', async () => {
+    test('Should throw survey questions not found or survey answers not found error', async () => {
       try {
-        await QuestionManager.deleteSurveyById(invalidSurveyId);
+        await CompositorManager.deleteSurvey(
+          testsValues.questionsValues.invalidSurveyId
+        );
       } catch (err) {
-        expect(err).toBeInstanceOf(SurveyNotFoundError);
+        expect(err).toBeInstanceOf(
+          SurveyQuestionsNotFoundError || SurveyAnswersNotFoundError
+        );
+      }
+    });
+  });
+
+  describe('Get survey results', () => {
+    test('Should get survey`s questions and answers', async () => {
+      const createdSurvey = await createSurvey(
+        {},
+        testsValues.questionsValues.validSurveyName1,
+        testsValues.questionsValues.validCreatorId,
+        testsValues.questionsValues.validContent1 as IQuestion[],
+        {
+          answersContent: testsValues.answersValues.validSurvey
+            .content as Array<ISection>,
+          userId: testsValues.answersValues.validSurvey.userId as string,
+        } as Omit<ISurveyAnswers, 'surveyId'>
+      );
+
+      if (!createdSurvey) {
+        fail();
+      }
+
+      const foundSurveyResults = await CompositorManager.getSurveyResults(
+        (createdSurvey[0] as ISurveyQuestions).id!
+      );
+      expect((createdSurvey[0] as ISurveyQuestions).id).toEqual(
+        (foundSurveyResults[0] as ISurveyQuestions).id
+      );
+    });
+
+    test('Should throw survey questions not found or survey answers not found error', async () => {
+      try {
+        await CompositorManager.getSurveyResults(
+          testsValues.questionsValues.invalidSurveyId
+        );
+      } catch (err) {
+        expect(err).toBeInstanceOf(
+          SurveyQuestionsNotFoundError || SurveyAnswersNotFoundError
+        );
       }
     });
   });
---
+++ src/utils/mocks.ts
@@ -0,0 +1,36 @@
+export const testsValues = {
+  // TODO: may have to change valid ID's (because id gets created during the creation step)
+  questionsValues: {
+    validSurveyName1: 'survey123',
+    validCreatorId: 'asdfasdfasdfasdfasdfasd1',
+    validContent1: [
+      {
+        questionName: 'is uri a dolphin?',
+        questionType: 'select',
+        answers: [
+          {
+            answer: 'yes',
+          },
+          {
+            answer: 'only half',
+          },
+        ],
+      },
+    ],
+    invalidSurveyId: '634e8da10c68790b062e0cf2',
+  },
+  answersValues: {
+    validSurvey: {
+      surveyId: '123412341234123412341234', // have to change according to
+      userId: '123456123456123456123456',
+      content: [
+        {
+          questionId: '111111111111111111111111',
+          answers: ['123456781234567812345678', '63510acd07d3e2e85fd642e1'],
+        },
+      ],
+    },
+    validSurveyId: '123412341234123412341234',
+    invalidSurveyId: '635139dd307506b573d447b',
+  },
+};
